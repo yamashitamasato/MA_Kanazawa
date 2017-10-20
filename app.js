96c6615d73c9e67ee29fea75adc569ade5b6752c
yamashitamasato <b1415966@planet.kanazawa-it.ac.jp>
2017-10-20 15:37:46 +0900

    a

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-// 1.モジュールオブジェクトの初期化
-var fs = require("fs");
-var server = require("https").createServer(function(req, res) {
-     res.writeHead(200, {"Content-Type":"text/html"});
-     var output = fs.readFileSync("./index.html", "utf-8");
-     res.end(output);
-}).listen(process.env.PORT || 3000);
-var io = require("socket.io").listen(server);
-
-// ユーザ管理ハッシュ
-var userHash = {};
-
-// 2.イベントの定義
-io.sockets.on("connection", function (socket) {
-
-  // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
-  socket.on("connected", function (name) {
-    var msg = name + "が入室しました";
-    userHash[socket.id] = name;
-    io.sockets.emit("publish", {value: msg});
-  });
-
-  // メッセージ送信カスタムイベント
-  socket.on("publish", function (data) {
-    io.sockets.emit("publish", {value:data.value});
-  });
-
-  // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
-  socket.on("disconnect", function () {
-    if (userHash[socket.id]) {
-      var msg = userHash[socket.id] + "が退出しました";
-      delete userHash[socket.id];
-      io.sockets.emit("publish", {value: msg});
-    }
-  });
+var express = require('express');
+var app = express.createServer();
+var ejs = require('ejs');
+var io = require('socket.io');
+var port = process.env.PORT || 3000;
+app.configure(function() {
+var expressStatic = express.static(__dirname + '/static');
+app.use(expressStatic);
 });
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
+app.set('transports', ['xhr-polling']);
+app.set('polling duration', 10);
+app.get('/', function(req, res) {
+console.log('/');
+res.render('index', { locals: { port: port } });
+});
+app.listen(port);
+var socket = io.listen(app);
+socket.on('connection', function(client) {
+client.on('message', function(msg) {
+console.log('send :' + msg);
+var sanitized = escapeHTML(msg);
+client.send(sanitized);
+client.broadcast(sanitized);
+});
+client.on('disconnect', function() {
+console.log('disconnect');
+});
+});
+function escapeHTML(str) {
+return str.replace(/&/g, "&amp;").replace(/"/g, "&quot;").replace(//g, "&gt;");
+console.log('Server running at ' + port + '/');

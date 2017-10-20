21d48c5ba1a20249b68acb823b02b539539f90f3
yamashitamasato <b1415966@planet.kanazawa-it.ac.jp>
2017-10-20 15:10:26 +0900

    sample

--- app.js
+++ app.js
@@ -1,37 +1,28 @@
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-// 1.モジュールオブジェクトの初期化
-var fs = require("fs");
-var server = require("http").createServer(function(req, res) {
-     res.writeHead(200, {"Content-Type":"text/html"});
-     var output = fs.readFileSync("./index.html", "utf-8");
-     res.end(output);
-}).listen(8080);
-var io = require("socket.io").listen(server);
+app.use(express.static(__dirname + "/"))
 
-// ユーザ管理ハッシュ
-var userHash = {};
+var server = http.createServer(app)
+server.listen(port)
 
-// 2.イベントの定義
-io.sockets.on("connection", function (socket) {
+console.log("http server listening on %d", port)
 
-  // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
-  socket.on("connected", function (name) {
-    var msg = name + "が入室しました";
-    userHash[socket.id] = name;
-    io.sockets.emit("publish", {value: msg});
-  });
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
 
-  // メッセージ送信カスタムイベント
-  socket.on("publish", function (data) {
-    io.sockets.emit("publish", {value:data.value});
-  });
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
 
-  // 接続終了組み込みイベント(接続元ユーザを削除し、他ユーザへ通知)
-  socket.on("disconnect", function () {
-    if (userHash[socket.id]) {
-      var msg = userHash[socket.id] + "が退出しました";
-      delete userHash[socket.id];
-      io.sockets.emit("publish", {value: msg});
-    }
-  });
-});
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})

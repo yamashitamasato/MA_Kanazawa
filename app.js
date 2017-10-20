da6e9f36066a8658bec42a1331b6f65eda141411
yamashitamasato <b1415966@planet.kanazawa-it.ac.jp>
2017-10-20 15:02:13 +0900

    hello

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // 1.モジュールオブジェクトの初期化
 var fs = require("fs");
 var server = require("http").createServer(function(req, res) {
@@ -34,3 +35,11 @@ io.sockets.on("connection", function (socket) {
     }
   });
 });
+*/
+
+var http = require('http'); //httpモジュール呼び出し
+http.createServer(function (request, response) {
+    // リクエストを受けると以下のレスポンスを送信する
+    response.writeHead(200, {'Content-Type': 'text/plain'}); //レスポンスヘッダーに書き込み
+    response.end('Hello World\n'); // レスポンスボディに書き込み＆レスポンス送信を完了する
+}).listen(process.env.PORT || 8080); //公開ポートで待ち受け

b58b7aca66aaa9f87c98c40230ed7cbaa89f581e
yamashitamasato <b1415966@planet.kanazawa-it.ac.jp>
2017-10-22 01:11:44 +0900

    test

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var html = require('fs').readFileSync('index.html');
+var fs = require('fs');
+var path = require('path');
+//var html = require('fs').readFileSync('wait.html');
+var mime = {
+  ".html": "text/html",
+  ".css":  "text/css"
+  // 読み取りたいMIMEタイプはここに追記
+};
 var http = require('http').createServer(
     function (req, res) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(html);
 
+  if (req.url == '/') {
+    filePath = '/index.html';
+  } else {
+    filePath = req.url;
+  }
+  var fullPath = __dirname + filePath;
+
+  res.writeHead(200, {"Content-Type": mime[path.extname(fullPath)] || "text/plain"});
+  fs.readFile(fullPath, function(err, data) {
+    if (err) {
+      // エラー時の応答
+    } else {
+      res.end(data, 'UTF-8');
+    }
+  });
 
 });
 var io = require('socket.io')(http);
@@ -20,5 +40,5 @@ io.on(
                 io.emit('msg', count);
             }
         );
-    }
+    },
 );
